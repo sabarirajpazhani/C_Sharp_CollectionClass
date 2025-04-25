54b218b62c55dad38016f2c65b7ac8b50943a4fc
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-25 23:50:15 +0530

    Add project files.

---
+++ C_Sharp_CollectionClass/Arraylist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace C_Sharp_CollectionClass
+{
+    public class Arraylist
+    {
+        public static void arrayBasic()
+        {
+            ArrayList list = new ArrayList();
+            list.Add(1);
+            list.Add("Gislen");
+            list.Add('c');
+            list.Add(20.3);
+
+
+            foreach(var i in list)
+            {
+                Console.WriteLine(i+" ");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            arrayBasic();
+        }
+
+    }
+}
