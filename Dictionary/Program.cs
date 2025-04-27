a9dec8fec83827d7d0b80b1ff6eef55c0df77b0c
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-27 11:51:08 +0530

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace C_Sharp_CollectionsClass
+{
+    public class Program
+    {
+        public static void DictionaryDeclaeration()
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+
+            for(int i = 0; i < 3; i++)
+            {
+                int key = Convert.ToInt32(Console.ReadLine());
+
+                string value = Console.ReadLine();
+
+                dic.Add(key, value);
+            }
+
+            foreach(object obj in dic)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DictionaryDeclaeration();  
+        }
+    }
+}
