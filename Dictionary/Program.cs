6325c0feb5d9fdc915454ef1464ba8c386b778b5
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-27 12:19:30 +0530

    acessing the elememt using foreach and for loop

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -22,9 +22,34 @@ namespace C_Sharp_CollectionsClass
                 Console.WriteLine(obj);
             }
         }
+
+        public static void acessingElement()
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>()
+            {
+                {1, "Raj" },
+                { 2,"Ravi" },
+                {3, "Gislen" }
+            };
+
+            foreach(KeyValuePair <int, string> kvp in dic)
+            {
+                Console.WriteLine(kvp.Key +" - "+kvp.Value);
+            }
+
+            for(int i = 0; i < dic.Count; i++)
+            {
+                int key = dic.Keys.ElementAt(i);
+                string value = dic[key];
+
+                Console.WriteLine(key + " - "+ value);
+            }
+        }
         static void Main(string[] args)
         {
-            DictionaryDeclaeration();  
+            //DictionaryDeclaeration();  
+            acessingElement();
+
         }
     }
 }
